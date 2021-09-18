76f55f194aa89f02f07de99a8fa684d3cf7e0f68
ArzymKoteyko <artemiy.kotenko@yandex.ru>
2021-09-18 12:19:08 +0300

    first assembler program

---
+++ assembler_nasm/hello_nasm.asm
@@ -0,0 +1,15 @@
+ section .data
+msg db "Hello, nasm!\n"
+len equ $-msg 
+section .text
+global _start 
+_start:
+mov eax, 4
+mov ebx, 1
+mov ecx, msg
+mov edx, len
+int 80h
+mov eax, 1
+xor ebx, ebx
+int 80h
+
